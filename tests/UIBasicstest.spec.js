dcae5333d3be96ac73bb45e7982d8d7f1ef0eca3
Arabinda Mohanty <arabindamohanty41@gmail.com>
2025-06-15 02:52:40 +0530

    brower configuration

---
+++ tests/UIBasicstest.spec.js
@@ -0,0 +1,49 @@
+const {test, expect}=require ('@playwright/test');
+
+
+// test ('First Playwright test'function()
+// {
+//     //playwright code.
+
+
+// }
+// );
+
+// test ('First Playwright test',async function()
+// {
+//     //playwright code.
+
+
+// }
+// );
+
+//test ('First Playwright test',async ()=>
+//{
+    //playwright code.
+    //step1 : open browser
+    //step2 : enter u/p 2 seconds
+    //step3 : click on login button
+//}
+//);
+
+
+test ('Browser context Playwright test ',async ({browser})=>
+{
+//chrome  -plugins/cookeies
+const context =await browser.newContext();
+ const page =await context.newPage();
+ await page.goto('https://rahulshettyacademy.com/loginpagePractise');
+
+});
+
+//test.only ('Page Playwright test',async ({page})=>
+    test('Page Playwright test',async ({page})=>
+    {
+     await page.goto('https://www.google.com');
+     //get title  and then put assertion
+     console.log(await page.title());
+
+    await expect(page).toHaveTitle('Google');
+
+     
+    });
