b0e6191bdaa1ba3d03ec0be11927b073699f7401
Arabinda Mohanty <arabindamohanty41@gmail.com>
2025-06-15 13:42:29 +0530

    code added

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -1,7 +1,7 @@
 const {test, expect}=require ('@playwright/test');
 
 
-        test.only('Browser context',async ({page})=>
+        test('Browser context',async ({page})=>
             {
                 await page.goto('https://rahulshettyacademy.com/client');
                 await page .locator('#userEmail').fill("arabindamohanty41@gmail.com ");
@@ -13,5 +13,25 @@ const {test, expect}=require ('@playwright/test');
                 console.log(titles);
 
                        });
-    
-        
+    test.only('UI Controls',async ({page})=>
+        {
+            await page.goto("https://rahulshettyacademy.com/loginpagePractise");
+            //css type fill 
+            const userName = page.locator('#username');
+            const signInBtn = page.locator('#signInBtn');
+            const dropdown = page.locator('select.form-control');
+            await dropdown.selectOption('consult');
+            await page.locator(".radiotextsty").last().click();
+            await page.locator("#okayBtn").click();
+            console.log(await page.locator(".radiotextsty").last().isChecked());
+            await expect(page.locator(".radiotextsty").last()).toBeChecked();
+            await page.locator('#terms').click();
+            await expect(page.locator('#terms')).toBeChecked();
+
+            await page.locator('#terms').uncheck();
+           expect(await page.locator('#terms').isChecked()).toBeFalsy(); 
+
+
+           // await page.pause();
+        }
+    );
